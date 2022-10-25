be689055a469c50f09e0ef1f0bc962a39a30b136
Shapna <shapna088@gmail.com>
2022-10-26 04:55:44 +0600

    added blog ques

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>Blog</h2>
+        <div className='flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl text-pink-700 font-bold flex justify-center mt-8'>Question Answer</h2>
+                <div className="grid grid-cols-1 gap-4 p-12">
+                    <div>
+                        <h3 className='text-fuchsia-500  text-2xl font-semibold'>1. What is `cors`?</h3>
+                        <p className='text-1xl font-medium max-w-xl mx-auto my-4'></p>
+                    </div>
+                    <div>
+                        <h3 className='text-fuchsia-500  text-2xl font-semibold'>2. Why are you using `firebase`? What other options do you have to implement authentication?</h3>
+                        <p className='text-1xl font-medium max-w-xl mx-auto my-4'></p>
+                    </div>
+                    <div>
+                        <h3 className='text-fuchsia-500  text-2xl font-semibold'>3. How does the private route work</h3>
+                        <p className='text-1xl font-medium max-w-xl mx-auto my-4'></p>
+                    </div>
+                    <div>
+                        <h3 className='text-fuchsia-500  text-2xl font-semibold'>4. What is Node? How does Node work?</h3>
+                        <p className='text-1xl font-medium max-w-xl mx-auto my-4'></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
