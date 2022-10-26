af2d7c97d17bcc8124a1ae541dc4fc18a63c9696
Shapna <shapna088@gmail.com>
2022-10-27 04:07:44 +0600

    btn added

--- src/Components/Pages/CourseDetails/CourseDetails.js
+++ src/Components/Pages/CourseDetails/CourseDetails.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import { FaDownload } from 'react-icons/fa';
 
 const CourseDetails = () => {
     const details = useLoaderData();
     console.log(details);
     return (
         <div className='flex justify-center m-4'>
+
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="card-actions justify-end">
+                    <Link>
+                        <div className='flex gap-2'>
+                            <p>Download</p>
+                            <button><FaDownload></FaDownload></button>
+                        </div>
+                    </Link>
+                </div>
                 <figure><img src={details?.image} alt="Course" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{details?.name}</h2>
