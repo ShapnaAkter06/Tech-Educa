696686800a8ff7986fde48f5dc5d066d6dd210c2
Shapna <shapna088@gmail.com>
2022-10-27 20:10:09 +0600

    added reg form

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
     }
 
     return (
-        <div className='flex justify-center items-center pt-8'>
+        <div className='flex justify-center items-center p-8'>
             <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
                 <div className='mb-8 text-center'>
                     <h1 className='my-3 text-4xl font-bold'>Log in</h1>
