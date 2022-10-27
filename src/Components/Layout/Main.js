2d0defaa22f64be7237566f28b46d8a992b39aca
Shapna <shapna088@gmail.com>
2022-10-27 18:15:52 +0600

    remove unneseesary code

--- src/Components/Layout/Main.js
+++ src/Components/Layout/Main.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Footer/Footer';
 import Navbar from '../Pages/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            <Outlet></Outlet>q
         </div>
     );
 };
