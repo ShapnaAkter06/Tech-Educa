49e4fe00e817fdf39c61e5e97aab0c6a8d05fbba
Shapna <shapna088@gmail.com>
2022-10-27 14:51:34 +0600

    price added

--- src/Components/Pages/CheckOut/CheckOut.js
+++ src/Components/Pages/CheckOut/CheckOut.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 
 const CheckOut = () => {
-    const display = useLoaderData()
+    const display = useLoaderData();
     return (
         <div className='flex justify-center items-center mt-16'>
             <div className="card w-96 bg-base-100 shadow-xl image-full">
@@ -11,7 +10,8 @@ const CheckOut = () => {
                 <div className="card-body">
                     <h1 className='text-2xl mb-4'>Welcome To Tech Educa</h1>
                     <h2 className="card-title text-2xl"> Course name: {display?.name}</h2>
-                    <p className='text-2xl'>ID Number: {display?.id} </p>
+                    <p className="card-title text-2xl">Course Fee: ${display?.courseFee}</p>
+                    <p className='text-1xl'><small>ID Number: {display?.id}</small></p>
                 </div>
             </div>
         </div>
